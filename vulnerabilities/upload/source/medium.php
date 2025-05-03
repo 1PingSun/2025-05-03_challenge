a8f4052d2c6fde35c9e4950d29dec99362d588ca
fhsh-tong3 <11135120@fhsh.tp.edu.tw>
2025-05-04 01:10:40 +0800

    Fix: Dangerous File Extension high 02

--- vulnerabilities/upload/source/medium.php
+++ vulnerabilities/upload/source/medium.php
@@ -1,33 +1,28 @@
 <?php
 
-if( isset( $_POST[ 'Upload' ] ) ) {
-	// Where are we going to be writing to?
-	$target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
-	$target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
+if (isset($_POST['Upload'])) {
+	$upload_dir = realpath(DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads") . DIRECTORY_SEPARATOR;
 
-	// File information
-	$uploaded_name = $_FILES[ 'uploaded' ][ 'name' ];
-	$uploaded_type = $_FILES[ 'uploaded' ][ 'type' ];
-	$uploaded_size = $_FILES[ 'uploaded' ][ 'size' ];
+	// 安全取得上傳檔案資訊
+	$file_tmp  = $_FILES['uploaded']['tmp_name'];
+	$file_name = basename($_FILES['uploaded']['name']);
+	$file_size = $_FILES['uploaded']['size'];
+	$file_ext  = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+	$file_info = getimagesize($file_tmp); // 更安全的方式來驗證圖片
 
-	// Is it an image?
-	if( ( $uploaded_type == "image/jpeg" || $uploaded_type == "image/png" ) &&
-		( $uploaded_size < 100000 ) ) {
+	// 檢查是否為有效圖片
+	if ($file_info !== false && in_array($file_info['mime'], ['image/jpeg', 'image/png']) && $file_size < 100000) {
+		// 避免目錄穿越與非法檔名
+		$safe_file_name = preg_replace('/[^a-zA-Z0-9_\.-]/', '_', $file_name);
+		$target_path = $upload_dir . $safe_file_name;
 
-		// Can we move the file to the upload folder?
-		if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
-			// No
+		if (!move_uploaded_file($file_tmp, $target_path)) {
 			$html .= '<pre>Your image was not uploaded.</pre>';
+		} else {
+			$html .= "<pre>" . htmlspecialchars($safe_file_name, ENT_QUOTES, 'UTF-8') . " successfully uploaded!</pre>";
 		}
-		else {
-			// Yes!
-			$html .= "<pre>{$target_path} succesfully uploaded!</pre>";
-		}
-	}
-	else {
-		// Invalid file
-		$html .= '<pre>Your image was not uploaded. We can only accept JPEG or PNG images.</pre>';
+	} else {
+		$html .= '<pre>Your image was not uploaded. Only JPEG or PNG images under 100KB are allowed.</pre>';
 	}
 }
-
 ?>
